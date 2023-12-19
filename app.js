ae07ab8c9fd07dffc5b03d215e4e4565b23964c9
bhargav794 <abhargav345@gmail.com>
2023-12-19 22:37:06 +0530

    Styled and fixed error messeges

--- app.js
+++ app.js
@@ -1,61 +1,28 @@
 import express, { json } from 'express';
-import httpProxy from "http-proxy-middleware";
-import cors from 'cors';
 import axios from 'axios';
 import cheerio from 'cheerio';
 import qs from 'qs';
-import path from 'path';
-import url from "url";
 
 
 let scraper = "https://saveig.app/api/ajaxSearch";
 
-
 const app = express();
-
-let sharedVar = { };
-
-
-
-// const __filename = url.fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(__filename);
     app.use(express.json());
     app.use(express.urlencoded({extended: true}));
     app.use(express.static("src"));
-    
-  //  app.get('/', (req, res) => {
-  //     res.sendFile(path.join(__dirname, "src/index.htm"));
-  //     console.log(__dirname);
-  // });
 
   app.post("/setUrl", async (req, res) => {
     try{
     const userLink = req.body.newURL;
-      sharedVar.newLink = await getVideo(userLink);
+      const sharedVar = { };
+      Object.assign(sharedVar, await getVideo(userLink));
       console.log(sharedVar);
-      res.status(200).json(sharedVar);
+      res.json(sharedVar);
     } catch (err) {
       console.error(err);
       res.status(500).json({ error: "Internal Server Error" });
     }  
   });
-
-    //console.log(sharedVar);
-
-
-  /*app.get("/get-data", (req, res) => {
-    try {
-     //console.log(sharedVar);
-     console.log(res.send(sharedVar));
-     //console.log(res);
-
-      res.send(sharedVar);
-    } catch (errors) {
-      console.error(errors);
-
-    }
-  });*/
- 
 
   async function getVideo(link) {
     const data = {
@@ -80,14 +47,11 @@ let sharedVar = { };
       "User-Agent":
         "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/119.0",
     };
-    // console.log(data);
     return axios.post(scraper, qs.stringify(data), { headers }).then((res) => {
        const response = res.data;
        const html = response.data;
          const $ = cheerio.load(html);
          const filter1 = $(".download-items");
-         //console.log(filter1);
-         // TODO get thumbnail, place it on the screen and pass the download link into a btn.
          const thumbNail = filter1
            .find(".download-items__thumb")
            .find("img")
@@ -96,19 +60,11 @@ let sharedVar = { };
            .find(".download-items__btn")
            .find("a")
            .attr("href");
-         //console.log(mainData, thumbNail);
-         //sharedVar = mainData;
-         //console.log(sharedVar);
-        //console.log(JSON.stringify(finalD));
-         //return { mainData, thumbNail };
-         //sharedVar = JSON.stringify(mainData);
-         return mainData;
+         return { downloadLink: mainData, imgLink: thumbNail };
      }).catch((err) => {
        console.log(err);
      })
  };
-
- 
 
 const port = 3000;
 
--- src/src.js
+++ src/src.js
@@ -1,63 +1,66 @@
 
 const form = document.myForm;
 const inpLink = document.querySelector(".userLink");
-let link;
-const downloadLink = document.querySelector(".downloadLink");
 
-//console.log({form, inpLink});
+const downloadLink = document.querySelector(".downloadLink");
+const imgLink = document.querySelector(".thumbImg");
+const contDiv = document.querySelector(".video-download");
+const errElem = document.querySelector(".errors");
+
+let link;
 
 async function handleLink(e) {
   try {
     e.preventDefault();
     link = inpLink.value;
-    // TODO  validatate instagram URL.
-
+    // TODO  validatate instagram URL with errors.
     if (!link.includes("instagram")) {
       alert("Only instagram links are supported");
       this.reset();
       return;
     }
-
     await updateURL(link);
-    //await fetchFromProxy();
     this.reset();
   } catch (err) {
     console.error(err);
   }
 }
 
-async function updateURL(newURLS) {
+async function updateURL(oriLink) {
   try {
     const response = await fetch("/setUrl", {
       method: "POST",
       headers: {
         "content-type": "application/json",
       },
-      body: JSON.stringify({ newURL: newURLS }),
+      body: JSON.stringify({ newURL: oriLink }),
     })
     .then(respo => {
       return respo.json();
     })
     .then(data => {
-      const fD = data.newLink;
-      console.log(fD);
-      getDURL(fD);
+      const fD = data.downloadLink;
+      const tB = data.imgLink;
+      if(fD != null || tB != null) {
+        getDURL(fD, tB);
+      }
+      else{
+        throw new Error("Something went wrong please try again");
+      }
     })
-
-    //if (response.status == 200) await getURL();
-
-    console.log("link updated");
-    //console.log(response.body);
   } catch (err) {
+    errElem.innerHTML = `${err}`;
+    errElem.style.display = "block";
+    contDiv.style.display = "none";
     console.error(err); //make this an error visible to user.
   }
 }
 
-async function getDURL(dLink) {
-
+async function getDURL(dLink, iLink) {
+      imgLink.src = `${iLink}`;
       downloadLink.href = `${dLink}`;
-       downloadLink.style.display = "block";
-    
+      errElem.style.display = "none";
+      contDiv.style.display = "flex";
     }
 
 form.addEventListener("submit", handleLink);
